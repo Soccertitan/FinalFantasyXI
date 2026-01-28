c90e26571a025f84c6ffef2b2f813171554a05b9
Soccertitan <tristan.nowacki@outlook.com>
2026-01-27 21:03:43 -0800

    InputActions + GameplayTags
    Created the base inputs required for the game.

---
+++ Source/FinalFantasyXI/Private/Input/Action/ActionInputMappingContext.cpp
@@ -0,0 +1,41 @@
+﻿// Copyright Soccertitan 2025
+
+
+#include "Input/Action/ActionInputMappingContext.h"
+
+#include "EnhancedInputSubsystems.h"
+
+void UActionInputMappingContext::OnInitializeAction()
+{
+	Super::OnInitializeAction();
+	
+	EnhancedInputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetPlayerController()->GetLocalPlayer());
+	ensure(EnhancedInputSubsystem);
+}
+
+void UActionInputMappingContext::OnInputActionTriggered(const FInputActionValue& Value)
+{
+	Super::OnInputActionTriggered(Value);
+	
+	if (InputMappingContext && bAppliedContext == false)
+	{
+		EnhancedInputSubsystem->AddMappingContext(InputMappingContext, Priority, ContextOptions);
+		bAppliedContext = true;
+	}
+}
+
+void UActionInputMappingContext::OnInputActionCompleted(const FInputActionValue& Value)
+{
+	Super::OnInputActionCompleted(Value);
+
+	EnhancedInputSubsystem->RemoveMappingContext(InputMappingContext, ContextOptions);
+	bAppliedContext = false;
+}
+
+void UActionInputMappingContext::OnInputActionCanceled(const FInputActionValue& Value)
+{
+	Super::OnInputActionCanceled(Value);
+	
+	EnhancedInputSubsystem->RemoveMappingContext(InputMappingContext, ContextOptions);
+	bAppliedContext = false;
+}
---
+++ Source/FinalFantasyXI/Public/Input/Action/ActionInputMappingContext.h
@@ -0,0 +1,40 @@
+﻿// Copyright Soccertitan 2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CrysAction.h"
+#include "EnhancedInputSubsystemInterface.h"
+#include "ActionInputMappingContext.generated.h"
+
+class UEnhancedInputLocalPlayerSubsystem;
+class UInputMappingContext;
+
+/**
+ * Adds/Removes an Input Mapping Context when Triggered/Completed/Canceled.
+ */
+UCLASS(Abstract)
+class FINALFANTASYXI_API UActionInputMappingContext : public UCrysAction
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void OnInitializeAction() override;
+
+	virtual void OnInputActionTriggered(const FInputActionValue& Value) override;
+	virtual void OnInputActionCompleted(const FInputActionValue& Value) override;
+	virtual void OnInputActionCanceled(const FInputActionValue& Value) override;
+	
+private:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UInputMappingContext> InputMappingContext;
+	UPROPERTY(EditAnywhere)
+	int32 Priority = 1;
+	UPROPERTY(EditAnywhere)
+	FModifyContextOptions ContextOptions;
+	
+	UPROPERTY()
+	TObjectPtr<UEnhancedInputLocalPlayerSubsystem> EnhancedInputSubsystem;
+	
+	bool bAppliedContext = false;
+};
