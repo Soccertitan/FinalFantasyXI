49e5bc34a057c83de9781d86d9be0b224813cd2e
Soccertitan <tristan.nowacki@outlook.com>
2026-04-22 13:29:08 -0700

    Created a Subsystem for a global widget pool.

---
+++ Source/FinalFantasyXI/Public/UI/Widget/CrysWidgetPoolSubsystem.h
@@ -0,0 +1,37 @@
+﻿// Copyright Soccertitan 2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidgetPool.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "CrysWidgetPoolSubsystem.generated.h"
+
+/**
+ * A simple subsystem for pooling widgets that are used across different actors within the world. I.E Damage number popups,
+ * character names, etc.
+ */
+UCLASS()
+class FINALFANTASYXI_API UCrysWidgetPoolSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+	virtual void PreDeinitialize() override;
+	
+	/**
+	 * Gets an instance of a widget of the given class.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Subsystem", meta = (DeterminesOutputType = "WidgetClass"))
+	UUserWidget* GetOrCreateInstance(TSubclassOf<UUserWidget> WidgetClass);
+	
+	/** Return a widget object to the pool, allowing it to be reused in the future */
+	UFUNCTION(BlueprintCallable, Category = "Subsystem")
+	void Release(TArray<UUserWidget*> Widgets, bool bReleaseSlate = false);
+	
+private:
+	
+	UPROPERTY(Transient)
+	FUserWidgetPool WidgetPool;
+};
